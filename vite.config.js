1da87223c64b62af1707f713a192acbd741a7b72
kobezzza <kobezzza@gmail.com>
2026-03-23 02:01:31 +0300

    chore: Добавил vite-plugin-typescript-transform и настроил поддержку декораторов

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,27 @@
+import ts from "typescript";
 import { defineConfig } from "vite";
 
 import tsconfigPath from "vite-tsconfig-paths";
+import { vitePluginTypescriptTransform } from "vite-plugin-typescript-transform";
 
 export default defineConfig({
   plugins: [
-    tsconfigPath()
+    tsconfigPath(),
+
+    vitePluginTypescriptTransform({
+      enforce: "pre",
+
+      filter: {
+        files: {
+          include: /\.ts$/,
+        },
+      },
+
+      tsconfig: {
+        override: {
+          target: ts.ScriptTarget.ES2024,
+        },
+      },
+    }),
   ]
 });
