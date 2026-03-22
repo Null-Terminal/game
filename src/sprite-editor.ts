e5c1426aeba99ec281a52d38cc976e55e6827dcb
kobezzza <kobezzza@gmail.com>
2026-03-22 15:15:24 +0300

    chore: Рефакторинг на веб-компоненты

--- src/sprite-editor.ts
+++ src/sprite-editor.ts
@@ -1,1 +1,21 @@
-import "#sprite-editor/init";
+import styles from "#sprite-editor/styles.css?raw";
+import template from "#sprite-editor/template.html?raw";
+
+class SpriteEditor extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+  }
+
+  connectedCallback() {
+    this.#render();
+  }
+
+  #render() {
+    if (this.shadowRoot != null) {
+      this.shadowRoot.innerHTML = `<style>${styles}</style>${template}`;
+    }
+  }
+}
+
+customElements.define("sprite-editor", SpriteEditor);
