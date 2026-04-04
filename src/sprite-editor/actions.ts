0f1f178cc48ac26a8dd87363807fed72ecaa1e0a
kobezzza <kobezzza@gmail.com>
2026-04-04 14:14:59 +0300

    refactor: Рефакторинг логики рендеринга и сохранения

--- src/sprite-editor/actions.ts
+++ src/sprite-editor/actions.ts
@@ -1,6 +1,6 @@
 import { loadBuffer } from "#/file-loader";
-import { SpriteBuffer } from "#/sprite-buffer";
 
+import { RenderedSpriteBuffer } from "#/sprite-buffer";
 import type { SpriteEditor } from "#/sprite-editor";
 
 import { Sprite } from "#sprite-editor/sprite";
@@ -18,6 +18,10 @@ export class ActionHandlers extends Handlers<SpriteEditor> {
     settings.removeEventListener("submit", this.#onSubmit);
   }
 
+  clearGrid() {
+    this.parent.grid.innerHTML = "";
+  }
+
   async addToGrid() {
     const editor = this.parent;
 
@@ -31,28 +35,7 @@ export class ActionHandlers extends Handlers<SpriteEditor> {
     const width = this.asNum(editor.getSetting("width"));
     const height = this.asNum(editor.getSetting("height"));
 
-    const sprites = new Map<string, {image?: File, data?: Uint8Array}>;
-
-    const fileExt =  /\.[^.]*$/;
-
-    for (const file of sprite.files!) {
-      const fileName = file.name;
-
-      const key = fileName.replace(fileExt, "");
-
-      const desc = sprites.get(key) ?? {};
-
-      const ext = fileName.match(fileExt)?.[0];
-
-      if (ext === ".buffer") {
-        desc.data = await loadBuffer(file);
-
-      } else {
-        desc.image = file;
-      }
-
-      sprites.set(key, desc);
-    }
+    const sprites = await groupSprites();
 
     for (const sprite of sprites.values()) {
       if (sprite.image == null) {
@@ -65,17 +48,10 @@ export class ActionHandlers extends Handlers<SpriteEditor> {
         }
 
       } else {
-        const length = sprite.data[0]!;
+        const data = new RenderedSpriteBuffer(sprite.data);
 
-        let offset = 2;
-
-        for (let i = 0; i < length; i++) {
-          const spriteBuffer = new SpriteBuffer(sprite.data, offset);
-
-          console.log(spriteBuffer.x, spriteBuffer.y, spriteBuffer.width, spriteBuffer.height);
-          console.log(spriteBuffer.delay, spriteBuffer.id);
-
-          offset += spriteBuffer.BYTES_PER_ELEMENT;
+        for (let i = 0; i < data.size; i++) {
+          const spriteBuffer = data.at(i)!;
 
           editor.grid?.append(new Sprite(sprite.image, {
             x: spriteBuffer.x,
@@ -84,22 +60,43 @@ export class ActionHandlers extends Handlers<SpriteEditor> {
             width: spriteBuffer.width,
             height: spriteBuffer.height,
 
-            spriteId: spriteBuffer.id,
-            animationDelay: spriteBuffer.delay,
+            spriteId: spriteBuffer.spriteId,
+            animationDelay: spriteBuffer.animationDelay,
           }));
         }
       }
     }
-  }
 
-  clearGrid() {
-    this.parent.grid.innerHTML = "";
+    async function groupSprites() {
+      const sprites = new Map<string, {image?: File, data?: Uint8Array}>;
+
+      const fileExt =  /\.[^.]*$/;
+
+      for (const file of sprite.files!) {
+        const fileName = file.name;
+        const ext = fileName.match(fileExt)?.[0];
+
+        const groupKey = fileName.replace(fileExt, "");
+        const group = sprites.get(groupKey) ?? {};
+
+        if (ext === ".buffer") {
+          group.data = await loadBuffer(file);
+
+        } else {
+          group.image = file;
+        }
+
+        sprites.set(groupKey, group);
+      }
+
+      return sprites;
+    }
   }
 
   async playPreview() {
     const { player } = this.parent;
 
-    const { canvas, data } = this.mergeSprites();
+    const { canvas, data } = this.#mergeSprites();
 
     player.width = canvas.width;
     player.height = canvas.height;
@@ -117,64 +114,6 @@ export class ActionHandlers extends Handlers<SpriteEditor> {
     }
   }
 
-  mergeSprites() {
-    const sprites = Array.from(this.parent.grid.querySelectorAll("sprite-item")) as Sprite[];
-
-    // Вычисляем общую ширину и максимальную высоту
-    let totalWidth = 0;
-    let maxHeight = 0;
-
-    for (const { canvas } of sprites) {
-      totalWidth += canvas.width;
-      maxHeight = Math.max(maxHeight, canvas.height);
-    }
-
-    // Создаем результирующий canvas
-    const canvas = document.createElement("canvas");
-    canvas.width = totalWidth;
-    canvas.height = maxHeight;
-
-    const ctx = canvas.getContext("2d")!;
-
-    // Рисуем каждый canvas на результирующем
-    let currentX = 0;
-
-    const HEADER = 2;
-    const data = new Uint8Array(sprites.length * SpriteBuffer.BYTES_PER_ELEMENT + HEADER);
-
-    data[0] = sprites.length;
-    data[1] = 0xFF;
-
-    let offset = HEADER;
-
-    for (const sprite of sprites) {
-      // Создаем временный canvas, чтобы отрисовать туда изображение без сетки
-      const image = sprite.canvas.cloneNode() as HTMLCanvasElement;
-      sprite.draw(image.getContext("2d")!);
-
-      // Вычисляем позицию по Y для центрирования по вертикали
-      const y = (maxHeight - sprite.canvas.height) / 2;
-      ctx.drawImage(image, currentX, y);
-
-      const spriteBuffer = new SpriteBuffer(data, offset);
-      offset += spriteBuffer.BYTES_PER_ELEMENT;
-
-      spriteBuffer.x = -currentX;
-      spriteBuffer.y = -y;
-      spriteBuffer.width = image.width;
-      spriteBuffer.height = image.height;
-      spriteBuffer.delay = sprite.animationDelay;
-      spriteBuffer.id = sprite.spriteId;
-
-      console.log(spriteBuffer.x, spriteBuffer.y, spriteBuffer.width, spriteBuffer.height);
-      console.log(spriteBuffer.delay, spriteBuffer.id);
-
-      currentX += sprite.canvas.width;
-    }
-
-    return { canvas, ctx, data };
-  }
-
   renderGrid() {
     const editor = this.parent;
 
@@ -182,7 +121,7 @@ export class ActionHandlers extends Handlers<SpriteEditor> {
       return;
     }
 
-    const { canvas, data } = this.mergeSprites();
+    const { canvas, data } = this.#mergeSprites();
 
     const image = document.createElement("a");
 
@@ -193,7 +132,7 @@ export class ActionHandlers extends Handlers<SpriteEditor> {
     const buffer = document.createElement("a");
 
     buffer.download = `${image.download}.buffer`;
-    buffer.href = `data:application/octet-stream;base64,${data.toBase64()}`;
+    buffer.href = data.toDataURL();
     buffer.click();
   }
 
@@ -211,4 +150,10 @@ export class ActionHandlers extends Handlers<SpriteEditor> {
   readonly #onSubmit = (e: Event) => {
     e.preventDefault();
   };
+
+  #mergeSprites() {
+    return RenderedSpriteBuffer.mergeSprites(
+      Array.from(this.parent.grid.querySelectorAll("sprite-item")) as Sprite[]
+    );
+  }
 }
