cc287b99b17928e9161276c3d244b264ee29cf25
kobezzza <kobezzza@gmail.com>
2026-04-05 15:12:45 +0300

    feat: Добавил кнопки управления проигрыванием и другие улучшения

--- src/sprite-editor/animation-preview.ts
+++ src/sprite-editor/animation-preview.ts
@@ -1,9 +1,11 @@
 import { cache } from "#decorators/cache";
-
-import type { SpriteEditor } from "#/sprite-editor";
+import { RenderedSpriteBuffer } from "#sprite-buffer/rendered-sprite-buffer";
 
 import styles from "#sprite-editor/animation-preview/styles.css?raw";
 import template from "#sprite-editor/animation-preview/template.html?raw";
+
+import type { SpriteEditor } from "#/sprite-editor";
+import type { Sprite } from "#sprite-editor/sprite";
 
 import { ActionHandlers } from "#sprite-editor/animation-preview/actions";
 
@@ -13,19 +15,23 @@ export class AnimationPreview extends HTMLElement {
     return this.shadowRoot!.getElementById("controls")!;
   }
 
+  speed = 1;
+  spriteIndex = 0;
+  backgroundColor = "#1BBF68";
+
   @cache
-  get player(): HTMLCanvasElement {
+  get #ctx() {
+    return this.#player.getContext("2d")!;
+  }
+
+  @cache
+  get #player(): HTMLCanvasElement {
     return this.shadowRoot!.getElementById("player") as HTMLCanvasElement;
   }
 
-  get grid() {
-    return this.#editor.grid;
-  }
-
-  speed = 1;
-
   #editor!: SpriteEditor;
   #actionHandlers!: ActionHandlers;
+  #animationId = 0;
 
   constructor() {
     super();
@@ -53,6 +59,99 @@ export class AnimationPreview extends HTMLElement {
 
   disconnectedCallback() {
     this.#actionHandlers.destroy();
+  }
+
+  play() {
+    const { canvas, data } = this.#renderSprite();
+
+    this.resize(canvas.width, canvas.height);
+
+    let lastFrameTime = 0;
+
+    cancelAnimationFrame(this.#animationId);
+
+    const animate = (now?: number) => {
+      this.#animationId = requestAnimationFrame(animate);
+
+      if (now == null) {
+        return;
+      }
+
+      this.spriteIndex %= data.size;
+      const sprite = data.at(this.spriteIndex)!;
+
+      if (now - lastFrameTime >= sprite.animationDelay * this.speed) {
+        this.spriteIndex++;
+        this.spriteIndex %= data.size;
+
+        this.clear();
+        this.#ctx.drawImage(
+          canvas,
+          sprite.x,
+          sprite.y,
+          sprite.width,
+          sprite.height,
+          0,
+          canvas.height - sprite.height,
+          sprite.width,
+          sprite.height
+        );
+
+        lastFrameTime = now;
+      }
+    };
+
+    animate();
+  }
+
+  renderSprite(spriteIndex: number) {
+    this.pause();
+    this.clear();
+
+    const { canvas, data } = this.#renderSprite();
+
+    spriteIndex %= data.size;
+    const sprite = data.at(spriteIndex);
+
+    if (sprite != null) {
+      this.#ctx.drawImage(
+        canvas,
+        sprite.x,
+        sprite.y,
+        sprite.width,
+        sprite.height,
+        0,
+        canvas.height - sprite.height,
+        sprite.width,
+        sprite.height
+      );
+    }
+  }
+
+  pause() {
+    cancelAnimationFrame(this.#animationId);
+  }
+
+  stop() {
+    cancelAnimationFrame(this.#animationId);
+    this.spriteIndex = 0;
+    this.clear();
+  }
+
+  resize(width: number, height: number) {
+    this.#player.width = width;
+    this.#player.height = height;
+  }
+
+  clear() {
+    this.#ctx.fillStyle = this.backgroundColor;
+    this.#ctx.fillRect(0, 0, this.#player.width, this.#player.height);
+  }
+
+  #renderSprite() {
+    return RenderedSpriteBuffer.mergeSprites(
+      Array.from(this.#editor.grid.querySelectorAll("sprite-item")) as Sprite[]
+    );
   }
 
   #render() {
--- src/sprite-editor/animation-preview/actions.ts
+++ src/sprite-editor/animation-preview/actions.ts
@@ -1,13 +1,8 @@
-import { RenderedSpriteBuffer } from "#sprite-buffer/rendered-sprite-buffer";
-
 import { Handlers } from "#sprite-editor/handlers";
-import type { Sprite } from "#sprite-editor/sprite";
 
 import type { AnimationPreview } from "#sprite-editor/animation-preview";
 
 export class ActionHandlers extends Handlers<AnimationPreview> {
-  #animationId = 0;
-
   constructor(parent: AnimationPreview) {
     super(parent);
   }
@@ -25,44 +20,23 @@ export class ActionHandlers extends Handlers<AnimationPreview> {
   }
 
   play() {
-    const { player } = this.parent;
+    this.parent.play();
+  }
 
-    const { canvas, data } = RenderedSpriteBuffer.mergeSprites(
-      Array.from(this.parent.grid.querySelectorAll("sprite-item")) as Sprite[]
-    );
+  pause() {
+    this.parent.pause();
+  }
 
-    player.width = canvas.width;
-    player.height = canvas.height;
+  stop() {
+    this.parent.stop();
+  }
 
-    const ctx = player.getContext("2d")!;
-    ctx.fillStyle = "#1BBF68";
+  nextSprite() {
+    this.parent.renderSprite(++this.parent.spriteIndex);
+  }
 
-    let spriteIndex = 0;
-    let lastFrameTime = 0;
-
-    cancelAnimationFrame(this.#animationId);
-
-    const animate = (now?: number) => {
-      this.#animationId = requestAnimationFrame(animate);
-
-      if (now == null) {
-        return;
-      }
-
-      const sprite = data.at(spriteIndex)!;
-
-      if (now - lastFrameTime >= sprite.animationDelay * this.parent.speed) {
-        spriteIndex++;
-        spriteIndex %= data.size;
-
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        ctx.drawImage(canvas, sprite.x, sprite.y, sprite.width, sprite.height, 0, canvas.height - sprite.height, sprite.width, sprite.height);
-
-        lastFrameTime = now;
-      }
-    };
-
-    animate();
+  previousSprite() {
+    this.parent.renderSprite(--this.parent.spriteIndex);
   }
 
   protected initHandlers() {
