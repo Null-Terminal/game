280b763e40e154af47a3f25b1f6b265657907e47
kobezzza <kobezzza@gmail.com>
2026-04-24 17:50:41 +0300

    feat: Добавил модуль для загрузки спрайтов и удаления фонового цвета

---
+++ src/engine/sprite-loader.ts
@@ -0,0 +1,84 @@
+const spriteCache = new Map<string, Promise<HTMLCanvasElement>>();
+
+export interface LoadSpriteOptions {
+  tolerance?: number;
+}
+
+export function loadSprite(url: string, options: LoadSpriteOptions = {}): Promise<HTMLCanvasElement> {
+  const { tolerance = 0 } = options ?? {};
+
+  const cacheKey = [url, removeBackground, tolerance].join("_");
+
+  const fromCache = spriteCache.get(cacheKey);
+
+  if (fromCache != null) {
+    return fromCache;
+  }
+
+  const { promise, resolve, reject } = Promise.withResolvers<HTMLCanvasElement>();
+
+  spriteCache.set(cacheKey, promise);
+
+  const img = new Image();
+
+  img.onload = () => {
+    try {
+      const canvas = document.createElement("canvas");
+      canvas.width = img.width;
+      canvas.height = img.height;
+
+      const ctx = canvas.getContext("2d")!;
+      ctx.drawImage(img, 0, 0);
+
+      removeBackground(canvas, tolerance);
+      resolve(canvas);
+
+    } catch (err) {
+      reject(err);
+    }
+  };
+
+  img.onerror = () => {
+    reject(new Error(`Failed to load sprite ${url}`));
+  };
+
+  img.src = url;
+
+  return promise;
+}
+
+function removeBackground(canvas: HTMLCanvasElement, tolerance = 30) {
+  const ctx = canvas.getContext("2d")!;
+
+  const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+  const data = imageData.data;
+
+  if (data.length < 4) {
+    return;
+  }
+
+  // Определяем цвет фона из левого верхнего угла
+  const targetColor = {
+    r: data[0]!,
+    g: data[1]!,
+    b: data[2]!,
+    alpha: data[3]!,
+  };
+
+  if (targetColor.alpha === 0) {
+    return;
+  }
+
+  // Заливка фона
+  for (let i = 0; i < data.length; i += 4) {
+    const diffR = Math.abs(data[i]! - targetColor.r);
+    const diffG = Math.abs(data[i + 1]! - targetColor.g);
+    const diffB = Math.abs(data[i + 2]! - targetColor.b);
+
+    if (diffR <= tolerance && diffG <= tolerance && diffB <= tolerance) {
+      data[i + 3] = 0;
+    }
+  }
+
+  ctx.putImageData(imageData, 0, 0);
+}
