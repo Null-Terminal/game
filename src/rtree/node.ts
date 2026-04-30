38ba9047026e62469c413ed7ee979b718e1e68ab
kobezzza <kobezzza@gmail.com>
2026-05-01 02:35:52 +0300

    feat: Начал делать узел r-дерева

---
+++ src/rtree/node.ts
@@ -0,0 +1,21 @@
+import { alias, array, tuple, bintype, usize2, u8, bool } from "#/bindata";
+
+import { bbox, BBox } from "#/rtree/bbox";
+
+const node = tuple("RTreeNode", [
+  alias("bbox", bbox),
+  alias("children", array("children", usize2, 16)),
+  alias("parent", usize2),
+  alias("level", u8),
+  alias("leaf", bool),
+  bintype("data", 12)
+]);
+
+export class RTreeNode {
+  static SCHEME = node;
+  static BYTES_PER_ELEMENT = node.size;
+
+  static bbox(data: Float32Array, offset: number) {
+    return new BBox(data, offset + node.at.bbox);
+  }
+}
