64bcd64acf6b39ca113f2a812f4d2b2139085a8e
kobezzza <kobezzza@gmail.com>
2026-05-03 07:42:50 +0300

    polish: Причесал код, убрал касты к any

--- src/bindata/array.ts
+++ src/bindata/array.ts
@@ -1,13 +1,15 @@
+import { cast } from "#/tstools";
+
 import { type BinType } from "#/bindata/bintype";
 
-export function array<const T extends string, const S extends number, E extends BinType>(
-  type: T,
+export function array<const N extends string, const S extends number, E extends BinType>(
+  name: N,
   element: E,
   size: S,
-): BinType<T, Tb.Mult<E["size"], S>> {
+): BinType<N, Tb.Mult<E["size"], S>> {
   return {
-    type,
-    alias: type,
-    size: (element.size * size) as any
+    name,
+    size: cast(element.size * size),
+    alias: name
   };
 }
