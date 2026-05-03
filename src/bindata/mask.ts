35c2393230010744717e551b3ea32ad47e8e8ff5
kobezzza <kobezzza@gmail.com>
2026-05-03 08:04:39 +0300

    feat: Добавил поддержку масок на BigInt

--- src/bindata/mask.ts
+++ src/bindata/mask.ts
@@ -1,3 +1,4 @@
-export function mask(size: number) {
-  return (1 << size) - 1;
+export function mask<T extends number | bigint>(size: T): T;
+export function mask(size: number | bigint): number | bigint {
+  return typeof size === "number" ? (1 << size) - 1 : (1n << size) - 1n;
 }
