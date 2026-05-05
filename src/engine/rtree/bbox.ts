360fba477ff2db1247ec93991b67972bb50f002f
kobezzza <kobezzza@gmail.com>
2026-05-05 19:33:28 +0300

    polish: Поменял блоки местами и выделил константы для лучшей читаемости

--- src/engine/rtree/bbox.ts
+++ src/engine/rtree/bbox.ts
@@ -8,25 +8,11 @@ export const bbox = tuple("bbox", [
   alias("maxY", f32),
 ]);
 
+const { offsets32 } = bbox;
+
 export class BBox extends BinView {
   static readonly Scheme = bbox;
   static override readonly BYTES_PER_ELEMENT = BBox.Scheme.size;
-
-  getMinX(ptr32: number): number {
-    return this.view.floats[ptr32 + bbox.offsets32.minX]!;
-  }
-
-  getMinY(ptr32: number): number {
-    return this.view.floats[ptr32 + bbox.offsets32.minY]!;
-  }
-
-  getMaxX(ptr32: number): number {
-    return this.view.floats[ptr32 + bbox.offsets32.maxX]!;
-  }
-
-  getMaxY(ptr32: number): number {
-    return this.view.floats[ptr32 + bbox.offsets32.maxY]!;
-  }
 
   isNull(ptr32: number): boolean {
     // Bbox считается null, если оба max-а равны 0.
@@ -34,13 +20,29 @@ export class BBox extends BinView {
     return this.getMaxX(ptr32) === 0 && this.getMaxY(ptr32) === 0;
   }
 
+  getMinX(ptr32: number): number {
+    return this.view.floats[ptr32 + offsets32.minX]!;
+  }
+
+  getMinY(ptr32: number): number {
+    return this.view.floats[ptr32 + offsets32.minY]!;
+  }
+
+  getMaxX(ptr32: number): number {
+    return this.view.floats[ptr32 + offsets32.maxX]!;
+  }
+
+  getMaxY(ptr32: number): number {
+    return this.view.floats[ptr32 + offsets32.maxY]!;
+  }
+
   set(ptr32: number, minX: number, minY: number, maxX: number, maxY: number) {
     const { floats } = this.view;
 
-    floats[ptr32 + bbox.offsets32.minX] = minX;
-    floats[ptr32 + bbox.offsets32.minY] = minY;
-    floats[ptr32 + bbox.offsets32.maxX] = maxX;
-    floats[ptr32 + bbox.offsets32.maxY] = maxY;
+    floats[ptr32 + offsets32.minX] = minX;
+    floats[ptr32 + offsets32.minY] = minY;
+    floats[ptr32 + offsets32.maxX] = maxX;
+    floats[ptr32 + offsets32.maxY] = maxY;
   }
 
   calcArea(ptr32: number): number {
