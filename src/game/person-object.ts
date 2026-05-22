1e1db1084996a39aacb3e650fca617cfeb7d1355
kobezzza <kobezzza@gmail.com>
2026-05-22 20:40:49 +0300

    refactor: Перенес игрока в src/game и сделал MVP движения

---
+++ src/game/person-object.ts
@@ -0,0 +1,102 @@
+import { MotionObject, type Animations } from "#engine/motion-object";
+import { loadSprite } from "#engine/sprite-loader";
+import { SpriteAnimation } from "#/sprite-animation";
+
+import runAnimation from "#/sprites/run.animation.json";
+import run from "#/sprites/run.png";
+
+const image = await loadSprite(run, { removeBackground: true });
+
+export class PersonObject extends MotionObject {
+  static override animations = {
+    run: [image, new SpriteAnimation(runAnimation.sprites)],
+  } satisfies Animations;
+
+  declare readonly Animations: (typeof PersonObject)["animations"];
+
+  init() {
+    this.play(this.animations.run);
+
+    const SPEED = 300;
+    const JUMP_FORCE = -800;
+    const GRAVITY = 2500;
+
+    let vy = 0;
+    let isOnGround = true;
+
+    const keys = {
+      ArrowLeft: false,
+      ArrowRight: false,
+      Space: false
+    };
+
+    window.addEventListener("keydown", (e) => {
+      const key = e.code;
+
+      if (e.code in keys) {
+        keys[key as keyof typeof keys] = true;
+        e.preventDefault();
+      }
+    });
+
+    window.addEventListener("keyup", (e) => {
+      const key = e.code;
+
+      if (key in keys) {
+        keys[key as keyof typeof keys] = false;
+        e.preventDefault();
+      }
+    });
+
+    let lastTime = performance.now();
+
+    this.canvas.emitter.on(this.canvas.events.redraw, ([now]) => {
+      const delta = Math.min(0.006, now - lastTime / 1000);
+
+      lastTime = now;
+
+      // Горизонтальное движение
+      let dx = 0;
+
+      if (keys.ArrowRight) {
+        dx += SPEED * delta;
+      }
+
+      if (keys.ArrowLeft) {
+        dx -= SPEED * delta;
+      }
+
+      // Прыжок
+      if (keys.Space && isOnGround) {
+        vy = JUMP_FORCE;
+        isOnGround = false;
+        keys.Space = false;
+      }
+
+      if (keys.ArrowLeft) {
+        this.effects.flipX = true;
+      }
+
+      if (keys.ArrowRight) {
+        this.effects.flipX = false;
+      }
+
+      // Гравитация
+      vy += GRAVITY * delta;
+
+      const dy = vy * delta;
+
+      // Запоминаем позицию до движения
+      const oldY = this.y;
+
+      // Двигаем
+      this.move(dx, dy);
+
+      if (oldY + dy < this.y) {
+        vy = 0;
+      }
+
+      isOnGround = dy > 0 && this.y === oldY;
+    });
+  }
+}
