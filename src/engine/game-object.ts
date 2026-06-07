01a61584b1f5b4b22c390692bef17cce4c15697e
kobezzza <kobezzza@gmail.com>
2026-06-07 11:04:57 +0300

    polish: Рефакторинг и повышение читаемости

--- src/engine/game-object.ts
+++ src/engine/game-object.ts
@@ -16,11 +16,7 @@ export abstract class GameObject extends KindedObject {
   @cache
   static get animationEntries(){
     const entries = Object.entries(this.animations);
-
-    entries.forEach(([name, value]) => {
-      value.name = name;
-    });
-
+    entries.forEach(([name, value]) => value.name = name);
     return entries;
   }
 
@@ -91,8 +87,13 @@ export abstract class GameObject extends KindedObject {
 
     if ("bbox" in this.options) {
       this.bbox = this.options.bbox;
-      this.x = this.bbox[0];
-      this.y = this.bbox[1];
+      const [minX, minY, maxX, maxY] = this.bbox;
+
+      this.x = minX;
+      this.y = minY;
+
+      this.#width = maxX - minX;
+      this.#height = maxY - minY;
 
     } else {
       if ("x" in this.options) {
@@ -145,19 +146,12 @@ export abstract class GameObject extends KindedObject {
     this.#activeAnimation = selectedAnimation;
 
     const { effects } = this;
-    const { emitter, events } = this.canvas;
+    const { emitter } = this.canvas;
 
     let rendered = false;
 
-    // Для bbox жестко фиксируем геометрию
-    if (this.bbox != null) {
-      const [minX, minY, maxX, maxY] = this.bbox;
-
-      this.#width = maxX - minX;
-      this.#height = maxY - minY;
-
     // Для объекта без bbox фиксируем ширину и высоту по самому широкому спрайту
-    } else {
+    if (this.bbox == null) {
       this.#width = selectedAnimation.maxWidth * effects.scale;
       this.#height = selectedAnimation.maxHeight * effects.scale;
     }
@@ -167,25 +161,25 @@ export abstract class GameObject extends KindedObject {
 
       const sprite = animation.at(spriteIndex)!;
 
+      let x = this.x;
+      let y = this.y;
+
+      if (effects.flipX) {
+        x = -x - this.width;
+      }
+
+      if (effects.flipY) {
+        y = -y - this.height;
+      }
+
+      ctx.scale(effects.flipX ? -1 : 1, effects.flipY ? -1 : 1);
+
       if (this.bbox != null) {
         ctx.fillStyle = selectedAnimation.getPatternFrame(ctx, spriteIndex);
         ctx.fillRect(this.x, this.y, this.width, this.height);
 
       } else {
         const image = selectedAnimation.getSpriteFrame(spriteIndex, effects.scale);
-
-        let x = this.x;
-        let y = this.y;
-
-        if (effects.flipX) {
-          x = -x - this.width;
-        }
-
-        if (effects.flipY) {
-          y = -y - this.height;
-        }
-
-        ctx.scale(effects.flipX ? -1 : 1, effects.flipY ? -1 : 1);
         ctx.drawImage(image, x, y, image.width, image.height);
       }
 
@@ -195,7 +189,9 @@ export abstract class GameObject extends KindedObject {
         this.animation.emit(this.animation.events[selectedAnimation.name]!, sprite.spriteId);
       }
 
-      if (!this.isPaused() && (now - lastFrameTime >= sprite.duration / effects.speed)) {
+      const duration = sprite.duration / (animation.params.speed * effects.speed);
+
+      if (!this.isPaused() && (now - lastFrameTime >= duration)) {
         spriteIndex = (spriteIndex + 1) % animation.length;
         lastFrameTime = now;
       }
