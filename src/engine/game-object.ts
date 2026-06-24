6ef9f3d3bb052e695ebe3b6c846c41a6a1746fce
kobezzza <kobezzza@gmail.com>
2026-06-24 17:27:54 +0300

    feat: Добавил новый объект для статической геометрии

--- src/engine/game-object.ts
+++ src/engine/game-object.ts
@@ -1,5 +1,8 @@
 export { GameObject } from "#engine/game-object/game-object";
+
 export { BackgroundObject } from "#engine/game-object/background-object";
+export { StaticalObject } from "#engine/game-object/statical-object";
+
 export { MotionObject } from "#engine/game-object/motion-object";
 export { CollisionObject, CollisionStatus } from "#engine/game-object/collision-object";
 
---
+++ src/engine/game-object/statical-object.ts
@@ -0,0 +1,16 @@
+import type { Game } from "#engine/game";
+import type { PoolPointer } from "#engine/game-object-pool";
+
+import { GameObject } from "#engine/game-object/game-object";
+import type { GameObjectOptions } from "#engine/game-object/types";
+
+export abstract class StaticalObject extends GameObject {
+  override get redrawEvent() {
+    return this.canvas.events.static;
+  }
+
+  override create(game: Game, poolPointer: PoolPointer, opts?: GameObjectOptions) {
+    super.create(game, poolPointer, opts);
+    this.world.addToStaticWorld(this);
+  }
+}
