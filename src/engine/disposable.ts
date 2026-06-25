89a812b64c0c1ed09c4ef4b495122000cbb322fb
kobezzza <kobezzza@gmail.com>
2026-06-25 17:54:21 +0300

    feat: Добавил стандартный сигнал для отмены подписок и встроенный метод для создания микротасок

--- src/engine/disposable.ts
+++ src/engine/disposable.ts
@@ -1,13 +1,33 @@
 export abstract class Disposable {
   readonly #destructors: Function[] = [];
+  readonly #abortController = new AbortController();
+
+  get abortSignal() {
+    return this.#abortController.signal;
+  }
 
   register<T extends () => void>(destructor: T): T {
     this.#destructors.push(destructor);
     return destructor;
   }
 
+  nextTick(cb: () => void): () => void {
+    let executed = false;
+
+    queueMicrotask(() => {
+      if (!executed) {
+        cb();
+      }
+    });
+
+    return this.register(() => {
+      executed = true;
+    });
+  }
+
   destroy() {
     this.#destructors.splice(0, this.#destructors.length).forEach((destroy) => destroy());
+    this.#abortController.abort();
   }
 
   [Symbol.dispose]() {
