7f0a985a85156ff722a9a917b4ec331d9041b221
kobezzza <kobezzza@gmail.com>
2026-07-10 22:03:21 +0300

    polish: Улучшил экспорты и стилистические правки

--- src/engine/game.ts
+++ src/engine/game.ts
@@ -1,17 +1,19 @@
 import { Disposable } from "#engine/disposable";
-import type { RenderCanvas } from "#engine/game/render-canvas";
 
 import { Camera } from "#engine/game/camera";
 import { World, type WorldOptions } from "#engine/game/world";
+import type { RenderCanvas } from "#engine/game/render-canvas";
 
-export { World, type WorldOptions };
-export { RenderCanvas, type RenderCanvasOptions, type RenderPayload } from "#engine/game/render-canvas";
+export { World } from "#engine/game/world";
+export type { WorldOptions, Collision, WorldObject, WorldObjects } from "#engine/game/world";
+
+export { RenderCanvas } from "#engine/game/render-canvas";
+export type { RenderCanvasOptions, RenderPayload } from "#engine/game/render-canvas";
 
 export class Game extends Disposable {
-  readonly canvas: RenderCanvas;
-
   readonly camera: Camera;
   readonly world: World;
+  readonly canvas: RenderCanvas;
 
   constructor(renderCanvas: RenderCanvas, world: WorldOptions) {
     super();
