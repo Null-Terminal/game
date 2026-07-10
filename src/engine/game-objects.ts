46f71a2e7e609abcff9242d41109c9395143812a
kobezzza <kobezzza@gmail.com>
2026-07-11 00:01:10 +0300

    feat: Добавил новый суперкласс для интерактивных объектов

--- src/engine/game-objects.ts
+++ src/engine/game-objects.ts
@@ -5,5 +5,6 @@ export { StaticObject } from "#engine/game-objects/static-object";
 
 export { DynamicObject } from "#engine/game-objects/dynamic-object";
 export { MovableObject, CollisionStatus } from "#engine/game-objects/movable-object";
+export { InteractObject } from "#engine/game-objects/interact-object";
 
 export * from "#engine/game-objects/types";
