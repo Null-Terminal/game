661f65778de4a17e787141307c9f88ed05a2bfe7
kobezzza <kobezzza@gmail.com>
2026-07-12 00:39:06 +0300

    polish: Обновил сцену

--- src/game/floor-object.ts
+++ src/game/floor-object.ts
@@ -1,16 +1,16 @@
 import { GameObject } from "#engine/game-objects";
-import type { Composition, CompositionInstances } from "#engine/game-objects";
+import type { With, Refs } from "#engine/game-objects";
 
 import { WallObject } from "#game/wall-object";
 import { FillerObject } from "#game/filler-object";
 
 export class FloorObject extends GameObject {
-  static override readonly composition = {
+  static override readonly with = {
     wall: [WallObject, { bbox: [-Infinity, 130, Infinity, 135] }],
     filler: [FillerObject, { stretchWidth: true }]
-  } satisfies Composition;
+  } satisfies With;
 
-  override composition = {} as CompositionInstances<(typeof FloorObject)["composition"]>;
+  override refs = {} as Refs<(typeof FloorObject)["with"]>;
 
   init() {}
 }
